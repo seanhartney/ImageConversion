50d0c034d8e9755f8f85c54c31aff173f2d3f737
Sean Hartney <seanfhartney@gmail.com>
2024-07-09 13:30:15 -0500

    Finished Converter

--- Converter.py
+++ Converter.py
@@ -8,8 +8,29 @@ from PIL import Image # type: ignore
 Dirpath = r"C:\Users\16127\Desktop\EmployeeID"
 Files = os.listdir(Dirpath)
 
+# Creating CSV file 
+fields = ['Employee ID', 'Base64 Image', 'Number']
+
+# Empty array of rows for spreadsheet
+rows = []
+
+numEmployees = 0
+
+csvFileName = "ConvertedImages.csv"
+
 for File in Files:
     imgPath = os.path.join(Dirpath, File)
     with open(''+ imgPath + '', "rb") as f:
         encoded_image = base64.b64encode(f.read())
-    print(encoded_image)
+        numEmployees = numEmployees + 1
+        empID = File[:-5]
+        tempArray = [empID, encoded_image, numEmployees]
+        rows.append(tempArray)
+
+with open(csvFileName, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(fields)
+    csvwriter.writerows(rows)
+
+print(numEmployees)
+
