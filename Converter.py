d3f9ef58c59dfa66ea56c6a2998da3a4f7e345de
Sean Hartney <seanfhartney@gmail.com>
2024-07-09 12:23:03 -0500

    Converter Progress

--- Converter.py
+++ Converter.py
@@ -1,2 +1,11 @@
 import csv
+import base64
+import os
+#with open("9hm6q.jpeg", "rb") as f:
 
+directory = 'C:\Users\16127\StOlafCode\SeanCode\ImageConversion'
+
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f):
+        print(f)
