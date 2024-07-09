a231fce7fd9927bbcaeb45183317c9a5d43d36fa
Sean Hartney <seanfhartney@gmail.com>
2024-07-09 12:56:06 -0500

    File Reading + File Encoding Working

--- Converter.py
+++ Converter.py
@@ -1,11 +1,15 @@
 import csv
 import base64
 import os
-#with open("9hm6q.jpeg", "rb") as f:
 
-directory = 'C:\Users\16127\StOlafCode\SeanCode\ImageConversion'
+from PIL import Image # type: ignore
 
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    if os.path.isfile(f):
-        print(f)
+# Insert your own desired file and filepath 
+Dirpath = r"C:\Users\16127\Desktop\EmployeeID"
+Files = os.listdir(Dirpath)
+
+for File in Files:
+    imgPath = os.path.join(Dirpath, File)
+    with open(''+ imgPath + '', "rb") as f:
+        encoded_image = base64.b64encode(f.read())
+    print(encoded_image)
